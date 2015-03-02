be812eefa974fb03d91530ac3db870b6bca307f0
Marcel Müller <neikos@neikos.email>
2015-03-02 13:30:32 +0100

    Fixed bug if current_user_id does not exist

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,10 @@
 module UserHelper
   def current_user
-    @_current_user ||= session[:current_user_id] &&
-      User.find(session[:current_user_id])
+    begin
+      @_current_user ||= session[:current_user_id] &&
+        User.find(session[:current_user_id])
+    rescue ActiveRecord::RecordNotFound
+      session[:current_user_id] = nil
+    end
   end
 end
