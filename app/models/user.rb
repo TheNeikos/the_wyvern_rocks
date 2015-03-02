1e4e26e189dfed38a664e7518b51ed61eae0cfaf
Marcel Müller <neikos@neikos.email>
2015-03-02 02:04:01 +0100

    Added regex validation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
-  validates :email_address, presence: true, confirmation: true, uniqueness: true
+  validates :email_address, presence: true, confirmation: true, uniqueness: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :email_address_confirmation, presence: true
 
 end
