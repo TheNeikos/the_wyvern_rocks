168e1e004dec4dff677e48419614cad4225f9173
Marcel Müller <neikos@neikos.email>
2015-03-19 18:36:28 +0100

    Added column helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def column_do options = {columns: 8, offset: 2}, &block
+    render(
+      partial: 'shared/column',
+      locals: { options: options, block: block }
+    )
+  end
 end
