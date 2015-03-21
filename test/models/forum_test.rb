d852fac382eef69207848e618485683da463e553
Marcel Müller <neikos@neikos.email>
2015-03-21 04:32:53 +0100

    Added tests for forum

--- test/models/forum_test.rb
+++ test/models/forum_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ForumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not be able to create a forum without category" do
+    forum = Forum.new name: "Hurr Durr I'm a sheep", order: 0
+    assert_not forum.save, "Saved an invalid Forum"
+  end
+
+  test "should not be able to create a forum without a name" do
+    forum = Forum.new dashboard_category: dashboard_categories(:general), order: 1
+    assert_not forum.save, "Saved an invalid Forum"
+  end
+
+  test "should be able to create a forum" do
+    forum = Forum.new dashboard_category: dashboard_categories(:general), name: "Introductions!", order: 2
+    assert forum.save, "Did not save a valid Forum"
+  end
 end
