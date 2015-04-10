3136ae8f0c8757412e2c2b9ee1817ecb4721f170
Marcel Müller <neikos@neikos.email>
2015-04-10 20:09:39 +0200

    Added bbcode and divarea CKEditor plugin

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,8 @@
+CKEDITOR.editorConfig = function(config) {
+
+    config.toolbar = [
+        ["Source", "-", "Bold", "Italic", "Underline", "Strike", "-", "JustifyCenter"]
+    ];
+
+    config.extraPlugins = "bbcode,divarea";
+}
