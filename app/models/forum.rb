c765f95bb61e42a0fb6da439852491764938d3f5
Marcel Müller <neikos@neikos.email>
2015-04-12 13:24:41 +0200

    Added slug for forum.rb

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -5,4 +5,8 @@ class Forum < ActiveRecord::Base
   validates :dashboard_category, presence: true
   validates :name, presence: true
   validates :order, uniqueness:  { scope: :dashboard_category }, presence: true
+
+  def to_param
+    "#{self.id}-#{self.name.parameterize}"
+  end
 end
