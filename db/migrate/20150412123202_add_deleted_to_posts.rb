41577d42faf3ad7c339195d2ec84a4cd5b8dd820
Marcel Müller <neikos@neikos.email>
2015-04-12 14:37:00 +0200

    Added paranoia to Posts

---
+++ db/migrate/20150412123202_add_deleted_to_posts.rb
@@ -0,0 +1,6 @@
+class AddDeletedToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :deleted_at, :datetime
+    add_index :posts, :deleted_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314161007) do
+ActiveRecord::Schema.define(version: 20150412123202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,10 @@ ActiveRecord::Schema.define(version: 20150314161007) do
     t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
   end
+
+  add_index "posts", ["deleted_at"], name: "index_posts_on_deleted_at", using: :btree
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
